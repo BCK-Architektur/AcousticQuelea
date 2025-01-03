59ac2d507f205319245fef88ee24162c430181de
krishnatejjami <164870525+TejJami@users.noreply.github.com>
2025-01-03 06:50:12 +0100

    .

--- AcousticQuelea/AcousticQueleaInfo.cs
+++ AcousticQuelea/AcousticQueleaInfo.cs
@@ -18,10 +18,10 @@ namespace AcousticQuelea
         public override Guid Id => new Guid("4b4b41c4-0a34-4a8e-92cc-5bfe889ec4d9");
 
         //Return a string identifying you or your company.
-        public override string AuthorName => "";
+        public override string AuthorName => "Tej";
 
         //Return a string representing your preferred contact details.
-        public override string AuthorContact => "";
+        public override string AuthorContact => "sai.jami@stud.th-owl.de";
 
         //Return a string representing the version.  This returns the same version as the assembly.
         public override string AssemblyVersion => GetType().Assembly.GetName().Version.ToString();
--- AcousticQuelea/obj/Debug/net7.0-windows/AcousticQuelea.AssemblyInfo.cs
+++ AcousticQuelea/obj/Debug/net7.0-windows/AcousticQuelea.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("Description of AcousticQuelea")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+2c32b4693fcb766ca68b4fa62ae463ded24f5b4b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+8fcca69130e0eecbcab431682bedb9743afc28e1")]
 [assembly: System.Reflection.AssemblyProductAttribute("AcousticQuelea")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AcousticQuelea")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
